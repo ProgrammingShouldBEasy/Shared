18ac60147007e0e95fd5f2e85da460ce5fa6c690
Cain <caintaylor2015@gmail.com>
2020-04-13 09:39:52 -0500

    Test Commit

---
+++ SharedMVC/SharedMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SharedMVC.Startup))]
+namespace SharedMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
