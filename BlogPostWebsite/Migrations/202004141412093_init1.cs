a5d9b326358679c2761a3a7f1fc76f25fc62914b
varshaauti27 <56707382+varshaauti27@users.noreply.github.com>
2020-04-14 09:55:09 -0500

    Updated Migrations

---
+++ BlogPostWebsite/Migrations/202004141412093_init1.cs
@@ -0,0 +1,18 @@
+﻿namespace BlogPostWebsite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init1 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Posts", "AuthorId", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Posts", "AuthorId", c => c.Int(nullable: false));
+        }
+    }
+}
